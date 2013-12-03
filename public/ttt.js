f71107ea73940cdae9a3342a6fb1f03312ad54d9
alexreedhill <ahill1994@student.elgin.edu>
2013-12-03 15:30:39 -0600

    Basic angular setup with some default values

--- public/ttt.js
+++ public/ttt.js
@@ -1,3 +1,11 @@
+var ttt = angular.module('ttt', []);
+
+ttt.controller('TTTCtrl', ['$scope', function($scope) {
+	$scope.cell1 = 'X';
+	$scope.cell2 = 'X';
+	$scope.cell3 = 'O';
+}]);
+
 $(function() {
 	// $('h1').click(function() {
 	// 	alert('clicked');
