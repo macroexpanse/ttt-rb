d8ef22468c1912202f243f919564974450ec1cc3
alexreedhill <ahill1994@student.elgin.edu>
2013-12-03 16:52:10 -0600

    Rows, add value, no overwrite

--- public/ttt.js
+++ public/ttt.js
@@ -1,25 +1,35 @@
 var ttt = angular.module('ttt', []);
 
 ttt.controller('TTTCtrl', ['$scope', function($scope) {
-	$scope.cell1 = 'X';
-	$scope.cell2 = 'X';
-	$scope.cell3 = 'O';
-}]);
-
-$(function() {
-	// $('h1').click(function() {
-	// 	alert('clicked');
-	// });
+	$scope.rows = [
+		{'id': '0', 'cells' : [
+			{'id' : '0', 'value': ''}, 
+			{'id' : '1', 'value': ''}, 
+			{'id' : '2', 'value': ''}
+		]},
+		{'id': '1', 'cells' : [
+			{'id' : '3', 'value': ''}, 
+			{'id' : '4', 'value': ''}, 
+			{'id' : '5', 'value': ''}
+		]},
+		{'id': '2', 'cells' : [
+			{'id' : '6', 'value': ''}, 
+			{'id' : '7', 'value': ''}, 
+			{'id' : '8', 'value': ''}
+		]}
+	]
 
 	var turn = 'X';
-	
-	$('td').click(function() {
-		var id = this.id.replace('cell-', '');
-		$(this).text(turn);
-		if(turn == 'X') {
-			turn = 'O';
-		} else {
-			turn = 'X';
-		};
-	});
-});
+
+	$scope.addValue = function(rowId, cellId) {
+		var cell = $scope.rows[rowId].cells[cellId - (rowId * 3)]
+		if(cell.value === '') {
+			cell.value = turn;
+			if(turn === 'X') {
+				turn = 'O';
+			} else {
+				turn = 'X';
+			};
+		};	
+	}
+}]);
