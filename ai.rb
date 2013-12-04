ba6e752f263966bb6171985edd1606698610dc32
alexreedhill <ahill1994@student.elgin.edu>
2013-12-04 12:21:31 -0600

    Second move

--- ai.rb
+++ ai.rb
@@ -1,15 +1,41 @@
 class Ai
 
-  def first_move(game)
-    if [game[:cells][0], game[:cells][2], game[:cells][6], game[:cells][8]].include?('X')
-      game[:cells][4] = 'O'
+  def first_move(cells)
+    if [cells[0][:value],cells[2][:value],cells[6][:value],cells[8][:value]].include?('X')
+     cells[4][:value] = 'O'
     else
-      game[:cells][0] = 'O'
+     cells[0][:value] = 'O'
     end
-    return game 
   end
 
-  def second_move(game)
-    
+  def second_move(cells)
+    player_cells = get_player_cells(cells)
+    if danger?(player_cells)
+      cells = resolve_danger(cells, player_cells)
+    elsif cells[0][:value] == 'X' && cells[8][:value] == 'X'
+      cells[5][:value] = 'O'
+    end
+    return cells
   end
+
+  def danger?(player_cells)
+    player_cells[0][:row_id] == player_cells[1][:row_id]
+  end
+
+  def resolve_danger(cells, player_cells)
+    positions = player_cells.collect { |c| c[:position] }
+    [0,1,2].each do |position|
+      unless positions.include?(position)
+        new_cell_id = position + (player_cells[0][:row_id] * 3)
+        cells[new_cell_id][:value] = 'O'
+      end
+    end
+    return cells
+  end
+
+  def get_player_cells(cells)
+    player_cells = cells.collect { |c| c if c[:value] == 'X'}
+    player_cells.compact
+  end
+
 end
--- spec/ttt_spec.rb
+++ spec/ttt_spec.rb
@@ -8,22 +8,25 @@ def app
   Sinatra::Application
 end
 
-def default_game
-  { :cells => [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',]
-  }
+def default_cells
+  [
+    {:id => 0, :row_id => 0, :position => 0, :value => ''},
+    {:id => 1, :row_id => 0, :position => 1, :value => ''},
+    {:id => 2, :row_id => 0, :position => 2, :value => ''},
+    {:id => 3, :row_id => 1, :position => 0, :value => ''},
+    {:id => 4, :row_id => 1, :position => 1, :value => ''},
+    {:id => 5, :row_id => 1, :position => 2, :value => ''},
+    {:id => 6, :row_id => 2, :position => 0, :value => ''},
+    {:id => 7, :row_id => 2, :position => 1, :value => ''},
+    {:id => 8, :row_id => 2, :position => 2, :value => ''}
+  ]
 end
 
 describe 'Tic Tac Toe Service' do
   include Rack::Test::Methods
+
+  let(:ai) { Ai.new }
+  let(:cells) { default_cells.clone }
 
   it 'loads the home page' do
     get '/'
@@ -31,24 +34,37 @@ describe 'Tic Tac Toe Service' do
   end
 
   it 'recieves data from front end' do
-    get '/game.json', :rows => default_game
+    get '/game.json', :cells => cells
 
     last_response.should be_ok
   end
 
   it 'responds to first move if not in corner' do
-    game = default_game
-    ai = Ai.new
-    game[:cells][4] = 'X'
-    game = ai.first_move(game)
-    game[:cells][0].should == 'O'
+    cells[4][:value] = 'X'
+    new_cells = ai.first_move(cells)
+    cells[0][:value].should == 'O'
   end
 
   it 'responds to first move if in corner' do
-    game = default_game
-    ai = Ai.new
-    game[:cells][0] = 'X'
-    game = ai.first_move(game)
-    game[:cells][4].should == 'O'
+    cells[0][:value] = 'X'
+    new_cells = ai.first_move(cells)
+    cells[4][:value].should == 'O'
   end
+
+  it 'responds to second move if first and last corner taken' do
+    cells[0][:value] = 'X'
+    cells[8][:value] = 'X'
+    cells[4][:value] = 'O'
+    new_cells = ai.second_move(cells)
+    new_cells[5][:value].should == 'O'
+  end
+
+  it 'responds to second move if 2 Xs in same row' do
+    cells[6][:value] = 'X'
+    cells[8][:value] = 'X'
+    cells[4][:value] = 'O'
+    new_cells = ai.second_move(cells)
+    new_cells[7][:value].should == 'O'
+  end
+
 end
