2c175e01353aa57c0f0e54d8f54a47e3db73d4f7
alexreedhill <ahill1994@student.elgin.edu>
2013-12-27 22:38:55 -0600

    Fix move adjacent to ensure false values are not counted

--- lib/game.rb
+++ lib/game.rb
@@ -24,7 +24,7 @@ class Game
 
 	def self.select_adjacent_cells(cells, type, value)
 	  first_ai_cell = Game.select_player_cells(cells, 'O').first
-	  adjacent_cells = cells.select { |cell| cell.send(type) == first_ai_cell.send(type) && cell.value == value }
+	  adjacent_cells = cells.select { |cell| cell.send(type) == first_ai_cell.send(type) && cell.send(type) != false && cell.value == value }
 	end
 	
 end
