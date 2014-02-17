6b9baa34d1f3060744103fb0ed7bfffeb5b3fe96
alexreedhill <ahill1994@student.elgin.edu>
2014-02-17 15:11:56 -0600

    Rank moves

--- lib/game_state.rb
+++ lib/game_state.rb
@@ -1,9 +1,53 @@
 class GameState
-  attr_accessor :current_player, :cells, :moves
+  attr_accessor :current_player, :cells, :moves, :rank
 
   def initialize(current_player, cells)
     self.current_player = current_player
     self.cells = cells
     self.moves = []
+  end
+
+  def rank 
+    @rank ||= final_state_rank || intermediate_state_rank
+  end
+
+  def intermediate_state_rank
+    ranks = moves.collect { |game_state| game_state.rank }
+    if current_player == "X"
+      ranks.max
+    else
+      ranks.min
+    end
+  end
+
+  def final_state_rank
+    if final_state?
+      return 0 if draw?
+      winner == "X" ? 1 : -1
+    end
+  end
+
+  def final_state?
+    winner || draw?
+  end
+
+  def draw?
+    cells.compact.size == 9 && winner.nil?
+  end
+
+  def winner
+    @winner = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]].collect { |positions|
+        ( cells[positions[0]] == cells[positions[1]] &&
+          cells[positions[1]] == cells[positions[2]] &&
+          cells[positions[0]] || nil)
+      }.compact.first
   end
 end
--- spec/game_state_spec.rb
+++ spec/game_state_spec.rb
@@ -1,18 +1,23 @@
 require_relative '../ttt.rb'
 require_relative '../lib/game_state.rb'
+require_relative '../lib/game_tree.rb'
 require_relative '../lib/cell.rb'
 require_relative '../lib/board.rb'
 require 'spec_helper.rb'
 
 describe 'Game State Service' do
-  let(:game_state) { GameState.new }
   let(:cells) { Cell.create_default_cells }
-  let(:board) { Board.new('human_value' => 'O') }
+  let(:board) { Board.new('human_value' => 'X') }
+  let(:game_tree) { GameTree.new }
+  let(:initial_game_state) { game_tree.generate(board.human_value) }
 
-  it "initializes with current_player and cells" do
-   game_state = GameState.new(board.human_value, cells)
-   expect(game_state.moves).to eq []
+  it "initializes with no moves" do
+   expect(initial_game_state.moves).to eq []
   end
 
+  it 'calculates rank for initial game state' do
+    game_tree.generate_moves(initial_game_state) 
+    expect(initial_game_state.moves.first.rank).to eq 0
+  end
 end
 
